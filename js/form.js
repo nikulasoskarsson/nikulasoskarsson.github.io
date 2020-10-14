4b11be8e02e69b8f5fd8468b1d20d21b3c2e3599
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-14 14:26:55 +0200

    removing previous form behaviour

--- js/form.js
+++ js/form.js
@@ -1,21 +1,21 @@
-const submitButton = document.getElementById("submit");
-submitButton.onclick = e => {
-  e.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const subject = document.getElementById("efni").value;
-  const message = document.getElementById("skilabod").value;
-  const date = new Date();
+// const submitButton = document.getElementById("submit");
+// submitButton.onclick = e => {
+//   e.preventDefault();
+//   const name = document.getElementById("name").value;
+//   const email = document.getElementById("email").value;
+//   const subject = document.getElementById("efni").value;
+//   const message = document.getElementById("skilabod").value;
+//   const date = new Date();
 
-  db.collection("emails")
-    .add({
-      name: name,
-      email: email,
-      subject: subject,
-      message: message,
-      date: date
-    })
-    .then(() => {
-      alert("Your email has beem recived");
-    });
-};
+//   db.collection("emails")
+//     .add({
+//       name: name,
+//       email: email,
+//       subject: subject,
+//       message: message,
+//       date: date
+//     })
+//     .then(() => {
+//       alert("Your email has beem recived");
+//     });
+// };
